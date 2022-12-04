949f5a36d6f421ae11f090dc8628ff13faaa7ace
John Bradford <jwgbradford@gmail.com>
2022-12-04 17:21:44 +0000

    Day 4_2 working on sample

--- day_4.py
+++ day_4.py
@@ -24,6 +24,25 @@ def nested_pairs(data_list):
             nesting += 1
     return nesting
 
+def any_overlap(data_list):
+    overlap = 0
+    for pair_combo in data_list:
+        pair_one, pair_two = pair_combo.split(",")
+        one_pt1, one_pt2 = pair_one.split("-")
+        two_pt1, two_pt2 = pair_two.split("-")
+        if (            
+            int(one_pt1) <= int(two_pt1) and 
+            int(one_pt2) >= int(two_pt2) or
+            int(one_pt1) >= int(two_pt1) and 
+            int(one_pt2) <= int(two_pt2) or
+            int(one_pt1) <= int(two_pt1) and 
+            int(one_pt2) >= int(two_pt1) or
+            int(one_pt1) <= int(two_pt2) and 
+            int(one_pt2) >= int(two_pt2)
+        ):
+            overlap += 1
+    return overlap
 
-list_of_data = get_data('day_4.txt')
+list_of_data = get_data('day_4_sample.txt')
 print(nested_pairs(list_of_data))
+print(any_overlap(list_of_data))
