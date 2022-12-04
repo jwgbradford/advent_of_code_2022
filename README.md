a0730999d8d9ae0d84f39954e69074a9320a9339
John Bradford <jwgbradford@gmail.com>
2022-12-04 14:14:00 +0000

    working on sample

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Advent of Code 2022
 Written in python, see how far we get
+
+Day 1 - borrowed some code from last year to load data from txt file and parse
--- day_01.py
+++ day_01.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 def read_file(file):
     input_file = open(file, 'r')
     input_data = input_file.read()
@@ -18,7 +20,7 @@ def list_to_dict(list_data):
             index += 1
             dictionary_data[index] = []
         else:
-            dictionary_data[index].append(item)
+            dictionary_data[index].append(int(item))
     return dictionary_data
 
 def find_most_calories(dict_list):
@@ -32,7 +34,29 @@ def find_most_calories(dict_list):
             best_elf = elf
     return best_elf, most_calories
 
-starting_list = get_data('day_1.txt')
+def sort_by_calories(dict_list):
+    temp_dict = {k:sum(dict_list[k]) for k in dict_list}
+    sorted_dict = {k: v for k,v in sorted(temp_dict.items(), key=itemgetter(1))}
+    return sorted_dict
+
+def return_last_items(dict_list, number_of_items):
+    dict_len = len(dict_list)
+    key_elves = list(dict_list)[dict_len - number_of_items:]
+    sub_dict = {elf : dict_list[elf] for elf in key_elves}
+    return sub_dict
+
+def sum_dict(dict_list):
+    temp_sum = 0
+    for elf in dict_list:
+        temp_sum += dict_list[elf]
+    return temp_sum
+
+starting_list = get_data('day_1_sample.txt')
 calorie_dictionary = list_to_dict(starting_list)
 best_elf, most_calories = find_most_calories(calorie_dictionary)
 print(best_elf, ' ', most_calories)
+sorted_elves = sort_by_calories(calorie_dictionary)
+print(sorted_elves)
+most_elves = return_last_items(sorted_elves, 3)
+group_calories = sum_dict(most_elves)
+print(group_calories)
